18a4a0408ec30f20169e1bf09352cd3a4d90a2fc
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-12 13:59:03 +0530

    WIP

--- cmd/gitserver/server/server.go
+++ cmd/gitserver/server/server.go
@@ -2339,6 +2339,8 @@ func (s *Server) doClone(ctx context.Context, repo api.RepoName, dir common.GitD
 	logger.Info("repo cloned")
 	repoClonedCounter.Inc()
 
+	s.PerforceChangelistMappingQueue.push(&perforceChangelistMappingJob{repo: repo})
+
 	return nil
 }
 
@@ -2604,6 +2606,7 @@ func (s *Server) doRepoUpdate(ctx context.Context, repo api.RepoName, revspec st
 	select {
 	case <-done:
 		if err != nil {
+			s.Logger.Warn("Pushing to perforcechangelistmapping queue")
 			s.PerforceChangelistMappingQueue.push(&perforceChangelistMappingJob{repo: repo})
 		}
 		return errors.Wrapf(err, "repo %s:", repo)

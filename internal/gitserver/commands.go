e6abfb8af714de5f5ccb1f6f81c1af62ed9ede34
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2023-05-29 16:31:26 +0530

    sg generate

--- internal/gitserver/commands.go
+++ internal/gitserver/commands.go
@@ -1916,9 +1916,7 @@ func ParseCommitLogOutput(data []byte, nameOnly bool) ([]*wrappedCommit, error) 
 }
 
 func parseCommitLogOutput(data []byte, nameOnly bool) ([]*wrappedCommit, error) {
-	logger := log.Scoped("parseCommitLogOutput", "")
 	allParts := bytes.Split(data, []byte{'\x00'})
-	logger.Warn("allParts breakdown", log.Int("length", len(allParts)))
 	partsPerCommit := partsPerCommitBasic
 	if nameOnly {
 		partsPerCommit = partsPerCommitWithFileNames
